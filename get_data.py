1025f0521dc5e86279c7e7b796eaed916c9e97f8
Giyu <131558924+giyu51@users.noreply.github.com>
2023-05-31 15:08:32 +0400

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -4,49 +4,51 @@ from datetime import datetime
 import time
 import os
 import csv
-import json
 
-with open("./zabbix_conf.json", 'r') as file:
-    confFile = json.load(file)
-COMPANY_DOMAIN_NAME = confFile['COMPANY_DOMAIN_NAME']
-USERNAME =confFile['USERNAME']
-PASSWORD =confFile['PASSWORD']
-IMAGE_DIRECTORY =confFile['IMAGE_DIRECTORY']
-REQUESTS_PER_HOUR =confFile['REQUESTS_PER_HOUR']
+# Load data configuration from the JSON file
+with open("./zabbix_conf.json", 'r') as conf_file:
+    conf_file = json.load(conf_file)
+    data_conf = conf_file['Data Collection']
+COMPANY_DOMAIN_NAME = data_conf['COMPANY_DOMAIN_NAME']
+USERNAME = data_conf['USERNAME']
+PASSWORD = data_conf['PASSWORD']
+IMAGE_DIRECTORY = data_conf['IMAGE_DIRECTORY']
+REQUESTS_PER_HOUR = data_conf['REQUESTS_PER_HOUR']
 
-
-
-# Check if the IMAGE_DIRECTORY you specified is 
+# Check if the specified IMAGE_DIRECTORY exists, create it if not
 current_folder_items = os.listdir(".")
-if  not current_folder_items.__contains__(IMAGE_DIRECTORY.replace("/", "").replace(".", "")) :
+if not current_folder_items.__contains__(IMAGE_DIRECTORY.replace("/", "").replace(".", "")):
     print(f'-----IMAGE_DIRECTORY {IMAGE_DIRECTORY} is created')
     os.mkdir(IMAGE_DIRECTORY)
 print(f'-----IMAGE_DIRECTORY is set to {IMAGE_DIRECTORY}')
-
-
 
 # Set request configurations
 url = f'https://{COMPANY_DOMAIN_NAME}/api_jsonrpc.php'
 headers = {'content-type': 'application/json-rpc'}
 
-
 item_dict = {}
 
-with open('graphs.csv', 'r') as file :
+# Read item names and IDs from the CSV file and create a dictionary
+with open('graphs.csv', 'r') as file:
     reader = csv.reader(file)
     for eachLine in reader:
         if len(eachLine) == 0:  # if the line is empty, ignore that line.
             continue
         if_name = eachLine[0]
         if_id = eachLine[1]
-        item_dict.update({if_name:if_id})
+        item_dict.update({if_name: if_id})
 
-#  In this script item_dict has almost no use, but in future if you want to modify the code, it could act as a link between interfaces' names and their items' ids
+# In this script item_dict has almost no use, but in the future if you want to modify the code,
+# it could act as a link between interfaces' names and their items' IDs
 item_ids = item_dict.values()
-        
+
+
+# Function to get the current time
 def current_time():
     return str(datetime.now().strftime('%d.%m.%y'))
 
+
+# Function to authenticate the user and get the session token
 def authenticate_user():
     auth_payload = {
         'jsonrpc': '2.0',
@@ -61,9 +63,11 @@ def authenticate_user():
     response = requests.post(url, data=json.dumps(auth_payload), headers=headers)
     result = json.loads(response.text)
     session_token = result['result']
-    print("Succesfully Logged IN")
+    print("Successfully Logged In")
     return session_token
 
+
+# Function to download images for the specified item IDs
 def download_images(session_token, item_ids):
     file_counter = len(os.listdir(IMAGE_DIRECTORY)) + 1
     image_headers = {
@@ -73,7 +77,7 @@ def download_images(session_token, item_ids):
 
     for item_id in item_ids:
         image_url = f"https://{COMPANY_DOMAIN_NAME}/chart.php?from=now-24h&to=now&itemids[0]={item_id}&type=0&profileIdx=web.item.graph.filter&profileIdx2=263949&legend=0&width=1334&widget_view=1"
-        
+
         try:
             response = requests.get(image_url, headers=image_headers)
         except:
@@ -84,9 +88,11 @@ def download_images(session_token, item_ids):
             with open('{0}/{1}_{2}.jpg'.format(IMAGE_DIRECTORY, current_time(), file_counter), 'wb') as f:
                 f.write(response.content)
             print(f'Image #{file_counter} saved successfully')
-        
+
         file_counter += 1
 
+
+# Function to logout the user
 def logout_user(session_token):
     logout_payload = {
         'jsonrpc': '2.0',
@@ -97,17 +103,19 @@ def logout_user(session_token):
     }
 
     requests.post(url, data=json.dumps(logout_payload), headers=headers)
-    print("Succesfully logged OUT")
-    
+    print("Successfully Logged Out")
 
+
+# Main function to execute the data collection process
 def main():
     while True:
         print("__________________________")
         session_token = authenticate_user()
         download_images(session_token, item_ids)
         logout_user(session_token)
-        
+
         time.sleep(3600 / REQUESTS_PER_HOUR)
+
 
 if __name__ == "__main__":
     main()
